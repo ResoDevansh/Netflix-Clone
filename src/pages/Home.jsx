c153f011f68a8a9b4c53c6ecde43ab59918ec3ed
ResoDevansh <sachandevansh2000@gmail.com>
2023-09-04 00:05:49 +0530

    more work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,50 @@
+import { useRef, useState, useEffect } from "react";
 import { styled } from "styled-components";
+import { validate } from "email-validator";
 import Navbar from "../components/Navbar";
 import bgImage from "../assets/home-background.jpg";
 import arrow from "../assets/arrow.png";
+import cross from "../assets/cross.png";
 
 const Home = () => {
+  const [email, setEmail] = useState("");
+  const isEmailValid = useRef(validate(email));
+  useEffect(() => {
+    isEmailValid.current = validate(email);
+  });
+  console.log(email, isEmailValid.current);
   return (
-    <Container backgroundImage={bgImage}>
+    <Container $backgroundImage={bgImage}>
       <Navbar />
       <FaceTag>
         <F1>
-          The biggest Indian hits. The best Indian stories. All
-          <br /> streaming here.
+          The biggest Indian hits. The best Indian stories. <br />
+          All streaming here.
         </F1>
         <F2>Watch anywhere. Cancel anytime.</F2>
         <F3>
           Ready to watch? Enter your email to create or restart your membership.
         </F3>
         <Form>
-          <EmailInput type="email" placeholder="Email address" />
+          <EmailInput
+            type="text"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
+          <MagicPlaceholder placeholder="Email address" className="magic">
+            Email address
+          </MagicPlaceholder>
           <Submit type="submit">
             <SubmitText>Get Started</SubmitText>
             <Arrow src={arrow} />
           </Submit>
+          <Validate $visible={isEmailValid.current}>
+            <Cross src={cross} />
+            {isEmailValid.current && "Please enter a valid email address."}
+          </Validate>
         </Form>
       </FaceTag>
     </Container>
@@ -30,12 +53,13 @@ const Home = () => {
 
 const Container = styled.div`
   height: 101.8vh;
-  background-image: ${(props) => `url(${props.backgroundImage})`};
+  background-image: ${(props) => `url(${props.$backgroundImage})`};
   background-size: fill;
   background-position: -15vw -18em;
   -webkit-box-shadow: inset 0px 20px 120px 61px rgba(0, 0, 0, 0.75);
   -moz-box-shadow: inset 0px 20px 120px 61px rgba(0, 0, 0, 0.75);
   box-shadow: inset 0px 20px 120px 61px rgba(0, 0, 0, 0.75);
+  z-index: 1;
 `;
 
 const FaceTag = styled.div`
@@ -56,7 +80,7 @@ const F2 = styled.div`
   font-size: 50%;
   font-weight: normal;
   margin-top: 0.5em;
-  margin-bottom: 0.7em;
+  margin-bottom: 1em;
 `;
 const F3 = styled.div`
   text-align: center;
@@ -64,26 +88,35 @@ const F3 = styled.div`
   font-weight: normal;
 `;
 const Form = styled.form`
+  position: relative;
   display: flex;
   justify-content: center;
   margin-top: 0.5em;
+  /* background-color: peachpuff; */
 `;
 const EmailInput = styled.input`
   color: white;
-  padding: 1em;
-  width: 30em;
+  padding: 0.6em;
+  padding-top: 1.3em;
+  padding-left: 0.8em;
+  width: 22em;
   position: relative;
-  left: -2em;
+  left: -0.8em;
   border: 1px solid red;
   background-color: rgb(10, 10, 10, 0.6);
   border-radius: 0.3em;
-  font-size: 0.3em;
-  &::placeholder {
-    color: #dfd3d0;
-    font-size: 1.2em;
+  font-size: 0.4em;
+  &:focus ~ .magic,
+  &:not(:focus):valid ~ .magic {
+    /* &:not(:focus):valid ~ .magic { */
+    left: 31.2vw;
+    font-size: 0.23em;
+    transform: translateY(-2em);
   }
 `;
 const Submit = styled.button`
+  display: flex;
+  align-items: center;
   padding: 0.7em 1.3em;
   border-radius: 0.3em;
   cursor: pointer;
@@ -91,14 +124,12 @@ const Submit = styled.button`
   text-decoration: none;
   border: transparent;
   color: white;
-  display: inline-flex;
-  align-items: center;
   transition: background-color 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
   &:hover {
     background-color: #d90505;
   }
   position: relative;
-  left: -1.3em;
+  left: -0.5em;
 `;
 const Arrow = styled.img`
   height: 1.5em;
@@ -107,5 +138,35 @@ const Arrow = styled.img`
 const SubmitText = styled.div`
   font-size: 1.7em;
   font-weight: 500;
+`;
+const MagicPlaceholder = styled.span`
+  font-size: 0.3em;
+  font-weight: initial;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0;
+  position: absolute;
+  left: 31vw;
+  bottom: 1.4em;
+  pointer-events: none;
+  transition: all 200ms ease;
+`;
+const Validate = styled.div`
+  color: #d82805;
+  display: flex;
+  align-items: center;
+  font-size: 0.25em;
+  position: absolute;
+  font-weight: 500;
+  top: 6em;
+  left: 30.2vw;
+  visibility: ${(props) => (!props.$visible ? "visible" : "hidden")};
+`;
+const Cross = styled.img`
+  margin-right: 0.3em;
+  height: 1.2em;
+  border: 1px solid red;
+  border-radius: 50%;
 `;
 export default Home;
